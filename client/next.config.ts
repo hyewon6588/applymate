3c49592e29b65040443abf76c67e694f33e95c8a
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-04-22 04:18:25 -0400

    alias settings update

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
     return config;
   },
 };
